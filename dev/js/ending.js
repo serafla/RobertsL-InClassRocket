a6f3f61dd73f941a0e4fe9fad1cea46c01280af4
serafla <63385649+serafla@users.noreply.github.com>
2021-05-11 06:06:16 -0400

    updated ending

--- dev/js/ending.js
+++ dev/js/ending.js
@@ -3,6 +3,7 @@ import { Power4 } from "gsap/all";
 
 export let endingTL = gsap.timeline();
 
+
 endingTL.to("#space-ship",{y:"-=400", scale: 3, ease: Power4, duration: 2, rotation: 120, onStart:controlFlames}, "beans")
 .to("#moon", {y:"+=200", scale: 0.5, duration: 2, alpha: 0}, "beans")
 .to("#space-ship", {y:"+=800", scale: 1, duration: 1.25, rotation: 400}, "pork")
@@ -16,6 +17,7 @@ flamesTL.to("#left-blue",{duration:0.25, scaleY:.25,yoyo:true, repeat:-1},"flame
 .to("#left-dark-blue",{duration:0.15, scaleY:.15,yoyo:true, repeat:-1},"flames")
 .to("#right-blue",{duration:0.25, scaleY:.25,yoyo:true, repeat:-1},"flames")
 .to("#right-dark-blue",{duration:0.15, scaleY:.15,yoyo:true, repeat:-1},"flames")
+
 
 function controlFlames(){
     gsap.set(".flames",{display:"block"});
